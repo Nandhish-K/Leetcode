7a23093b3b212a7fe57e539e7fd28510df972c72
nandhishk12 <1nh22ai101@newhorizonindia.edu>
2025-09-07 15:59:17 +0530

    Time: 522 ms (14.11%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select author_id as id from Views where
+select distinct author_id as id from Views where
 Views.author_id = Views.viewer_id 
-group by author_id
+
 order by author_id
