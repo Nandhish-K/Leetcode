bd0b9a374096232d09998266689a72d0d447bc6c
nandhishk12 <1nh22ai101@newhorizonindia.edu>
2025-09-11 11:22:09 +0530

    Time: 71 ms (84.41%), Space: 20.4 MB (65.37%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -1,22 +1,37 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        lower = []
-        upper = []
+        # lower = []
+        # upper = []
+        # vowels = {"A","E","I","O","U","a","e","i","o","u"}
+        # for ch in s:
+        #     if ch.isupper() and ch in vowels:
+        #         upper.append(ch)
+        #     elif ch.islower() and ch in vowels:
+        #         lower.append(ch)
+        # upper.sort()
+        # lower.sort()
+        # combination = upper + lower
+        # res = ""
+        # i = 0
+        # for ch in s:
+        #     if ch not in vowels:
+        #         res +=ch
+        #     else:
+        #         res +=combination[i]
+        #         i+=1
+        # return res
         vowels = {"A","E","I","O","U","a","e","i","o","u"}
+        res = []
         for ch in s:
-            if ch.isupper() and ch in vowels:
-                upper.append(ch)
-            elif ch.islower() and ch in vowels:
-                lower.append(ch)
-        upper.sort()
-        lower.sort()
-        combination = upper + lower
-        res = ""
+            if  ch in vowels:
+                res.append(ch)
+        res.sort()
+        result = ""
         i = 0
         for ch in s:
-            if ch not in vowels:
-                res +=ch
+            if ch in vowels:
+                result += res[i]
+                i += 1
             else:
-                res +=combination[i]
-                i+=1
-        return res
+                result += ch
+        return result
