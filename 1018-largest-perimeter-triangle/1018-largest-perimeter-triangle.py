f6d757cc1598a460b02872e2faf4be42a973fd94
nandhishk12 <1nh22ai101@newhorizonindia.edu>
2025-09-28 10:35:10 +0530

    Time: 7 ms (97.69%), Space: 19.1 MB (6.54%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
@@ -1,8 +1,19 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        res = 0
-        nums.sort()
-        for i in range(len(nums)-2) :
-            if nums[i] + nums[i+1] > nums[i+2] and nums[i] + nums[i+2] > nums[i+1] and nums[i+1] + nums[i+2] > nums[i] :
-                res = max(res,nums[i] + nums[i+1] + nums[i+2])
-        return res 
+        # res = 0
+        # nums.sort()
+        # for i in range(len(nums)-2) :
+        #     if nums[i] + nums[i+1] > nums[i+2] and nums[i] + nums[i+2] > nums[i+1] and nums[i+1] + nums[i+2] > nums[i] :
+        #         res = max(res,nums[i] + nums[i+1] + nums[i+2])
+        # return res 
+        nums = [-num for num in nums]
+        heapq.heapify(nums)
+        while len(nums) >= 3 :
+            a = -heapq.heappop(nums)
+            b = -heapq.heappop(nums)
+            c = -nums[0]
+
+            if b + c > a :
+                return a + b + c
+            heapq.heappush(nums,-b)
+        return 0
