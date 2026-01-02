43263063f0c872008ff3749b494b671dc4eeed25
Nandhish K <1nh22ai101@newhorizonindia.edu>
2026-01-02 12:23:31 +0530

    Time: 0 ms (100%), Space: 47.7 MB (25.57%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+    //      Map<Integer, Integer> dict = new HashMap<>();
+    //      for (int x : nums) {
+    //         dict.put(x, dict.getOrDefault(x, 0) + 1);
+    //     }
+    //     int num_frequency = nums.length/2;
+    //     for(int k:dict.keySet()){
+    //         if(dict.get(k) == num_frequency){
+    //             return k;
+    //         }
+    //     }
+    // return -1;
+    Set<Integer> s = new HashSet<>();
+    for(int i:nums){
+        if(!s.add(i)){
+            return i;
+        }
+    }
+    return nums[nums.length-1];
+
+    }
+}
