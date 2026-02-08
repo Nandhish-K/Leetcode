5a1b8c803518667bad9ee2426fffb6a780510966
Nandhish K <1nh22ai101@newhorizonindia.edu>
2026-02-08 11:38:58 +0530

    Time: 409 ms (41.35%), Space: 20.1 MB (49.19%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -15,14 +15,27 @@ class Solution:
         #     if d[k] < minimum :
         #         minimum = d[k]
         # return minimum
-        a_count_right = [0] * len(s)
-        for i in range(len(s) - 2,-1,-1) :
-            a_count_right[i] = a_count_right[i+1]
-            a_count_right[i] += 1 if s[i+1] == 'a' else 0
+        # a_count_right = [0] * len(s)
+        # for i in range(len(s) - 2,-1,-1) :
+        #     a_count_right[i] = a_count_right[i+1]
+        #     a_count_right[i] += 1 if s[i+1] == 'a' else 0
+        # b_count_left = 0
+        # res = len(s)
+        # for i,c in enumerate(s) :
+        #     res = min(res,b_count_left + a_count_right[i])
+        #     if c == 'b' :
+        #         b_count_left += 1
+        # return res
+        a_count_right = 0
+        for c in s :
+            a_count_right += 1 if c == 'a' else 0
+        
         b_count_left = 0
         res = len(s)
         for i,c in enumerate(s) :
-            res = min(res,b_count_left + a_count_right[i])
+            if c == 'a' :
+                a_count_right -= 1
+            res = min(res,b_count_left + a_count_right)
             if c == 'b' :
                 b_count_left += 1
         return res
